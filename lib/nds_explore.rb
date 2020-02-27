3f7e97bdac299399eece9a33a5b75052df77475d
Garen Sahagian <g.s.sahagian@gmail.com>
2020-02-27 16:55:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ end
 def print_first_directors_movie_titles
   index_count=0 
   while directors_database[index_count] do
-     puts directors_database[0][:movies][index_count][:title]
+     puts directors_database[0][1][index_count][1]
     index_count+=1 
   end
 end
